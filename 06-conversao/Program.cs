2490a51c8855a2ada037fbba577f505e5e5aad7a
guilhermesaldanha02 <guilhermesaldanha007@gmail.com>
2026-01-07 00:42:21 -0300

    conversao numerica

---
+++ 06-conversao/Program.cs
@@ -0,0 +1,9 @@
+﻿//Obter o primeiro numero
+Console.WriteLine("=== Informe um numero ===");
+int numero1 = Convert.ToInt16(Console.ReadLine());
+
+//Obter o segundo numero
+Console.WriteLine("=== Informe outro numero ===");
+int numero2 = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine($"A soma é: {numero1 + numero2}");
