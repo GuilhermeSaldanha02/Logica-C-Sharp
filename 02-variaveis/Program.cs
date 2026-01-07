92d623596bca410dd54d1cd3c6975067fb00a783
guilhermesaldanha02 <guilhermesaldanha007@gmail.com>
2026-01-07 00:29:46 -0300

    Estrutura de variaveis

---
+++ 02-variaveis/Program.cs
@@ -0,0 +1,21 @@
+﻿// Textos
+string nome = "Guilherme";
+
+//Letras
+char incial = 'G';
+
+//Números inteiros
+int idade = 25;
+
+//Números reais
+double altura = 1.80;
+
+//Lógicas
+bool estudante = true;
+
+//Exibir os valosres na tela
+Console.WriteLine(nome);
+Console.WriteLine(incial);
+Console.WriteLine(idade);
+Console.WriteLine(altura);
+Console.WriteLine(estudante);
