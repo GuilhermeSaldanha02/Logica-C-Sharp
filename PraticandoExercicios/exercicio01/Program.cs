310c92fd171c99288f4f631ebdb43fa33c63631d
guilhermesaldanha02 <guilhermesaldanha007@gmail.com>
2026-01-08 22:11:15 -0300

    exercicios praticos 01-02-03

---
+++ PraticandoExercicios/exercicio01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // === PRODUTO 1 ---
+        string nome = "Arroz integral";
+        decimal preco = 12.50m;
+        int estoque = 150;
+        double peso = 1.5;
+        bool emPromocao = true;
+        char categoria = 'A';
+        DateTime dataValidade = new DateTime(2026, 06, 15);
+
+        Console.WriteLine("=== CADASTRO DE PRODUTO ===");        
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Preço: {preco.ToString("C")}");
+        Console.WriteLine($"Quantidade em estoque: {estoque} unidades"); 
+        Console.WriteLine($"Peso: {peso} kg");
+        Console.WriteLine($"Em promoção: {(emPromocao ? "SIM" : "NÃO")}");
+        Console.WriteLine($"Categoria: {categoria}");
+        Console.WriteLine($"Data de Validade: {dataValidade.ToString("dd/MM/yyyy")}");
+
+
+        // === PRODUTO 2 ===
+        var nome2 = "Feijão Preto";
+        var preco2 = 8.90m;        
+        var estoque2 = 50;          
+        var peso2 = 1.0;            
+        var emPromocao2 = false;    
+        var categoria2 = 'A';       
+        var dataValidade2 = new DateTime(2026, 11, 30); 
+
+        Console.WriteLine("=== CADASTRO DE PRODUTO 2 ===");
+        Console.WriteLine($"Nome: {nome2}");
+        Console.WriteLine($"Preço: {preco2.ToString("C")}");
+        Console.WriteLine($"Quantidade em estoque: {estoque2} unidades"); 
+        Console.WriteLine($"Peso: {peso2} kg");      
+        Console.WriteLine($"Em promoção: {(emPromocao2 ? "SIM" : "NÃO")}"); 
+        Console.WriteLine($"Categoria: {categoria2}");
+        Console.WriteLine($"Data de Validade: {dataValidade2.ToString("dd/MM/yyyy")}");
+    }
+}
---
+++ PraticandoExercicios/exercicio03/Program.cs
@@ -0,0 +1,83 @@
+﻿Console.WriteLine("--------------------------------------------------");
+Console.WriteLine("== Loja de Produtos ==");
+
+string produto = "Leito";
+Console.WriteLine($"Produto: {produto}");
+
+int quantidadeEstoque = 50;
+Console.WriteLine($"Quantidade em estoque: {quantidadeEstoque}");
+
+int quantidadeSolicitada = 30;
+Console.WriteLine($"Quantidade solicitada: {quantidadeSolicitada}");
+
+int estoqueMinimo = 20;
+Console.WriteLine($"Estoque mínimo: {estoqueMinimo}");
+
+Console.WriteLine($"Quantidade disponível? {quantidadeSolicitada <= quantidadeEstoque}");
+Console.WriteLine($"Estoque abaixo do mínimo? {quantidadeEstoque < estoqueMinimo}");
+Console.WriteLine($"Estoque após venda abaixo do mínimo? {quantidadeEstoque - quantidadeSolicitada < estoqueMinimo}");
+
+Console.WriteLine("--------------------------------------------------");
+
+Console.WriteLine("== Loja de Promoções ==");
+string doce = "Chocolate";
+Console.WriteLine($"Doce: {doce}");
+
+decimal precoDoce = 8.50m;
+Console.WriteLine($"Preço do doce: R$ {precoDoce}");
+
+int quantidadeDoceEstoque = 100;
+Console.WriteLine($"Quantidade em estoque: {quantidadeDoceEstoque}");
+
+DateTime dataValida = new DateTime(2026, 02, 01);
+Console.WriteLine($"Validade: {dataValida}");
+Console.WriteLine($"Data atual: {DateTime.Now}");
+Console.WriteLine($"O produto está em promoção? {precoDoce < 10 && quantidadeDoceEstoque > 50 && (dataValida - DateTime.Now).Days > 30}");
+
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine("== Valdação de compra ==");
+
+decimal valorCompra = 250.00m;
+Console.WriteLine($"Valor da compra: R$ {valorCompra}");
+
+decimal valorPago = 300.00m;
+Console.WriteLine($"Valor pago: R$ {valorPago}");
+
+bool clienteVip = true;
+Console.WriteLine($"Cliente VIP? {clienteVip}");
+
+bool cupomDesconto = false;
+Console.WriteLine($"Possui cupom de desconto? {cupomDesconto}");
+
+Console.WriteLine($"Valor pago é suficiente? {valorPago >= valorCompra}");
+Console.WriteLine($"Cliente VIP ou possui cupom de desconto? {clienteVip || cupomDesconto}");
+Console.WriteLine($"Cliente é vip? {!clienteVip}");
+Console.WriteLine($"Cliente tem acesso a desconto especial? {clienteVip && cupomDesconto}");
+
+
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine("== Valdação complexa ==");
+
+string produtoEletronico = "Notebook Gamer";
+Console.WriteLine($"Produto: {produtoEletronico}");
+
+decimal precoEletronico = 4500.00m;
+Console.WriteLine($"Preço: {precoEletronico}");
+
+int estoqueEletronico = 5;
+Console.WriteLine($"Estoque: {estoqueEletronico}");
+
+int quantidadeSolicitada = 2;
+Console.WriteLine($"Quantidade Solicitada: {quantidadeSolicitada}");
+
+bool creditoCliente = true;
+Console.WriteLine($"Cliente tem credito? {creditoCliente}");
+
+decimal limiteCredito = 5000.00m;
+Console.WriteLine($"Qual limite de credito do cliente? {limiteCredito}");
+
+bool blackFriday = true;
+Console.WriteLine($"Está na Black Friday? {blackFriday}");
+
+Console.WriteLine($"Venda aprovada? { (estoqueEletronico >= quantidadeSolicitada) && ( (creditoCliente && limiteCredito >= precoEletronico * quantidadeSolicitada) || blackFriday ) }");
+
