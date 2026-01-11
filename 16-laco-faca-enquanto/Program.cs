af069389668f58d45cd26195d93ffdb2473c8730
guilhermesaldanha02 <guilhermesaldanha007@gmail.com>
2026-01-11 18:34:37 -0300

    16-laco-faca-enquanto

---
+++ 16-laco-faca-enquanto/Program.cs
@@ -0,0 +1,24 @@
+﻿// //Variavel
+// Console.WriteLine("==Qual a porcentagem da sua bateria?==");
+// double bateria = double.Parse(Console.ReadLine());
+
+// //laço de repetição faça enquanto
+
+// do
+// {
+//     Console.WriteLine($"sua bateria atual é {bateria}");
+//     bateria--;    
+// }
+// while(bateria >= 0);
+
+//Praticando
+
+Console.WriteLine("== Conte de 10 até 0 ==");
+double numero = double.Parse(Console.ReadLine());
+
+do
+{
+    Console.WriteLine($"Estou contanto o numero {numero}");
+    numero--;
+}
+while(numero >= 0);
