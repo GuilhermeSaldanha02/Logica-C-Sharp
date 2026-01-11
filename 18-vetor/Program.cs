7d9a29f3f8cb965f7905a99b7b41ed4f9873c7f3
guilhermesaldanha02 <guilhermesaldanha007@gmail.com>
2026-01-11 18:36:41 -0300

    18-vetor

---
+++ 18-vetor/Program.cs
@@ -0,0 +1,13 @@
+﻿//Vetor
+String[] nomes = {"Guilherme", "Bruna", "Filipe", "Marcelo"};
+
+//Exibir Nomes
+// Console.WriteLine(nomes[0]);
+// Console.WriteLine(nomes[1]);
+// Console.WriteLine(nomes[2]);
+// Console.WriteLine(nomes[3]);
+
+for(int indice = 0; indice < 4; indice++)
+{
+    Console.WriteLine(nomes[indice]);
+}
