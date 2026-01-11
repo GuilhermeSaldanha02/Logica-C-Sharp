93a2d6400fa3b703cca75be7d056262cebc74ad9
guilhermesaldanha02 <guilhermesaldanha007@gmail.com>
2026-01-11 18:33:11 -0300

    15-laco-enquanto

---
+++ 15-laco-enquanto/Program.cs
@@ -0,0 +1,20 @@
+﻿//Variavel
+Console.WriteLine("Qual o numero?");
+int indice = int.Parse(Console.ReadLine());
+
+while(indice <= 10)
+{
+    Console.WriteLine(indice);
+    indice++;
+}
+
+
+//Praticando
+Console.WriteLine("Conte até de 20 até 30");
+int contador = int.Parse(Console.ReadLine());
+
+while(contador <= 30)
+{
+    Console.WriteLine(contador);
+    contador++;
+}
