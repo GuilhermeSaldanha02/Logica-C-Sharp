ee780af3ff249449a0945c1c28c6a416af5cd1c1
guilhermesaldanha02 <guilhermesaldanha007@gmail.com>
2026-01-11 22:17:14 -0300

    exercicio05

---
+++ PraticandoExercicios/exercicio04/Program.cs
@@ -0,0 +1,135 @@
+﻿Console.WriteLine("----------------------------------------------");
+
+Console.WriteLine("SISTEMA DE DESCONTOS:");
+decimal valorCompra = 350m;
+Console.WriteLine($"Valor original: {valorCompra:C}");
+
+
+if(valorCompra > 500)
+{
+    decimal valor20 = valorCompra*0.20m;
+    Console.WriteLine($"Desconto Aplicado: 20%");
+    Console.WriteLine($"Valor do Desconto: {valor20:C}");  
+    Console.WriteLine($"Valor Final: {valorCompra-valor20:C}");  
+}
+else if(valorCompra >= 300)
+{
+    decimal valor15 = valorCompra*0.15m;
+    Console.WriteLine($"Desconto Aplicado: 15%");
+    Console.WriteLine($"Valor do Desconto: {valor15:C}");
+    Console.WriteLine($"Valor Final: {valorCompra-valor15:C}");
+}
+else if(valorCompra >= 100)
+{
+    decimal valor10 = valorCompra*0.10m;
+    Console.WriteLine($"Desconto Aplicado: 10%");
+    Console.WriteLine($"Valor do Desconto: {valor10:C}");
+    Console.WriteLine($"Valor Final: {valorCompra-valor10:C}");
+}
+else
+{
+    Console.WriteLine("Sem desconto!");
+    Console.WriteLine($"Valor da compra: {valorCompra:C}");
+}
+
+Console.WriteLine("----------------------------------------------");
+Console.WriteLine("SISTEMA DE CLASSIFICAÇÃO:");
+string nomeCliente = "Maria Silva";
+Console.WriteLine($"Nome do cliente: {nomeCliente}");
+
+decimal gastoMes = 2800.00m;
+Console.WriteLine($"Total gasto no mês: {gastoMes:C}");
+
+bool tempoCliente = true;
+Console.WriteLine($"É cliente a mais de 1 ano: {tempoCliente}");
+
+
+if(gastoMes >= 5000.00m)
+{
+    Console.WriteLine("Cliente Diamente: 25% de desconto + frete grátis");
+}
+else if (gastoMes > 2000.00m && gastoMes < 5000.00m && tempoCliente == true)
+{
+    Console.WriteLine("Cliente Ouro: 15% de desconto + frete grátis");
+}
+else if (gastoMes > 1000.00m && gastoMes < 1999.99m)
+{
+    Console.WriteLine("Cliente Prata: 10% de desconto");
+}
+else
+{
+    Console.WriteLine("Cliente Bronze: 5% de desconto");
+}
+
+Console.WriteLine("----------------------------------------------");
+Console.WriteLine("SISTEMA DE VALIDAÇÃO DE VENDA:");
+
+string produtoTv = "Smart TV 50 polegadas";
+Console.WriteLine ($"Produto: {produtoTv}");
+
+decimal precoTv = 2200.00m;
+Console.WriteLine($"Preço: {precoTv:C}");
+
+int estoqueTv = 3;
+Console.WriteLine($"Estoque: {estoqueTv}");
+
+int quantidadeSolicitada = 2;
+Console.WriteLine($"Quantidade: {quantidadeSolicitada}");
+
+bool creditoCliente = true;
+Console.WriteLine($"Cliente tem limite de crédito: {creditoCliente}");
+
+decimal limiteDisponivel = 3000.00m;
+Console.WriteLine($"Limite disponivel: {limiteDisponivel:C}");
+
+string[] formaPagamento = {"dinheiro","debito","credito","pix"};
+Console.WriteLine($"Forma de pagamento: {formaPagamento[2]}");
+
+decimal totalCompra = (precoTv*quantidadeSolicitada);
+Console.WriteLine($"Total da compra{totalCompra:C}");
+
+if (quantidadeSolicitada > estoqueTv) 
+{
+    Console.WriteLine("VENDA RECUSADA: Não temos estoque suficiente.");
+}
+else if (totalCompra > limiteDisponivel) 
+{
+    Console.WriteLine("VENDA RECUSADA: Limite do cartão insuficiente.");
+}
+else 
+{
+    Console.WriteLine("VENDA APROVADA: Parabéns pela compra!");
+}
+
+Console.WriteLine("----------------------------------------------");
+Console.WriteLine("SISTEMA DE ALERTA DE ESTOQUE:");
+
+string produtoGrao = "Arroz 5Kg";
+Console.WriteLine($"Produto: {produtoGrao}");
+
+int quantidadeGrao = 15;
+Console.WriteLine($"Quantidade de estoque: {quantidadeGrao}");
+
+int estoqueMinimoGrao = 20;
+Console.WriteLine($"Estoque mínimo: {estoqueMinimoGrao}");
+
+int estoqueMaximoGrao = 100;
+Console. WriteLine($"Estoque Máximo: {estoqueMaximoGrao}");
+
+int diasEntrega = 3;
+Console.WriteLine($"Dias para próxima entrega: {diasEntrega}");
+
+if (quantidadeGrao < estoqueMinimoGrao  && diasEntrega > 5)
+{
+Console.WriteLine("alerta: Critico");
+}
+else if (quantidadeGrao < estoqueMinimoGrao && diasEntrega <= 5)
+{
+Console.WriteLine("alerta: Atenção");
+}
+else if (quantidadeGrao >= estoqueMinimoGrao && quantidadeGrao <= estoqueMaximoGrao)
+{
+Console.WriteLine("alerta: Normal");   
+}
+else {Console.WriteLine("alerta: Estoque acima do maximo.");
+}
---
+++ PraticandoExercicios/exercicio05/Program.cs
@@ -0,0 +1,260 @@
+﻿int menu = 3;
+Console.WriteLine("Opção selecionada: " + menu);
+
+if (menu == 1)
+{Console.WriteLine("Cadastrar Produto:");}
+else if (menu == 2)
+{
+Console.WriteLine("Consultar Estoque");
+}
+else if(menu == 3)
+{
+Console.WriteLine("Emitir Relatorio");
+}
+else
+{
+Console.WriteLine("Sair");
+}
+
+Console.WriteLine("===================");
+Console.WriteLine("  Menu Principal  ");
+Console.WriteLine("===================");
+
+Console.WriteLine("1 - Cadastrar produto");
+Console.WriteLine("2 - Consultar estoque");
+Console.WriteLine("3 - Realizar venda");
+Console.WriteLine("4 - Emitir relatório");
+Console.WriteLine("5 - Sair");
+
+
+Console.Write("Selecione uma opção: ");
+int menu1 = int.Parse(Console.ReadLine());
+
+switch(menu1)
+{
+case 1:
+Console.WriteLine("Cadastrar Produto");
+break;
+
+case 2:
+Console.WriteLine("Consultar Estoque");
+break;
+
+case 3:
+Console.WriteLine("Realizar Venda");
+break;
+
+case 4:
+Console.WriteLine("Emitir Relatorio");
+break; 
+
+default:
+Console.WriteLine("Sair");
+break;
+}
+
+Console.WriteLine("===================");
+Console.WriteLine("  Categoriração  ");
+Console.WriteLine("===================");
+
+Console.WriteLine("A - Alimentos: Validade curta, requer refrigeração");
+Console.WriteLine("B - Bebidas: Validade média, armazenamento normal");
+Console.WriteLine("E - Eletrônicos: Sem validade, requer cuidado especial");
+Console.WriteLine("L - Limpeza: Validade longa, armazenamento normal");
+Console.WriteLine("H - Higiene: Validade média, armazenamento normal");
+
+Console.WriteLine("Escolha uma opção:");
+string opcao = Console.ReadLine().ToUpper();
+
+switch(opcao)
+{
+case "A":
+Console.WriteLine("Alimentos: Validade curta, requer refrigeração");
+break;
+
+case "B":
+Console.WriteLine("Bebidas: Validade média, armazenamento normal");
+break;
+
+case "E":
+Console.WriteLine("Eletrônicos: Sem validade, requer cuidado especial");
+break;
+
+case "L":
+Console.WriteLine("Limpeza: Validade longa, armazenamento normal");
+break;
+
+case "H":
+Console.WriteLine("Higiene: Validade média, armazenamento normal");
+break;
+
+default:
+Console.WriteLine("Opção invalida");
+break;
+}
+
+Console.WriteLine("===================");
+Console.WriteLine("  CÁLCULO DE IMPOSTO  ");
+Console.WriteLine("===================");
+
+string produto = "Notebook";
+Console.WriteLine("Produto: " + produto);
+
+string categoria = "Eletrônicos";
+Console.WriteLine("Categoria: " + categoria);
+
+decimal preco = 3000.00m;
+Console.WriteLine("Preço: R$ " + preco.ToString("C"));
+
+decimal impostoAlimentos = 0.05m;
+Console.WriteLine("Imposto sobre Alimentos: " + impostoAlimentos.ToString("P"));
+
+decimal impostoBebidas = 0.15m;
+Console.WriteLine("Imposto sobre Bebidas: " + impostoBebidas.ToString("P"));
+
+decimal impostoEletronicos = 0.25m;
+Console.WriteLine("Imposto sobre Eletrônicos: " + impostoEletronicos.ToString("P"));
+
+decimal impostoLivros = 0.00m;
+Console.WriteLine("Imposto sobre Livros: " + impostoLivros.ToString("P"));
+
+decimal impostoOutros = 0.18m;
+Console.WriteLine("Imposto sobre Outros: " + impostoOutros.ToString("P"));
+
+switch(categoria)
+{
+  case "alimentos":
+    preco += preco * impostoAlimentos;
+    Console.WriteLine("Preço com imposto: R$ " + preco.ToString("C"));
+    break;
+
+  case "bebidas":
+    preco += preco * impostoBebidas;    
+    Console.WriteLine("Preço com imposto: R$ " + preco.ToString("C"));
+    break;
+
+  case "Eletrônicos":
+    preco += preco * impostoEletronicos;
+    Console.WriteLine("Preço com imposto: R$ " + preco.ToString("C"));
+    break;
+
+  case "livros":
+    preco += preco * impostoLivros;
+    Console.WriteLine("Preço com imposto: R$ " + preco.ToString("C"));
+    break;
+
+  default:
+    preco += preco * impostoOutros;
+    Console.WriteLine("Preço com imposto: R$ " + preco.ToString("C"));
+    break;
+}
+Console.WriteLine("===================");
+Console.WriteLine("  Prazo de Entrega por Região  ");
+Console.WriteLine("===================");
+
+string regiao = Console.ReadLine().ToLower();
+Console.WriteLine("Região selecionada: " + regiao);
+
+int norte = 10;
+int nordeste = 8;
+int centroOeste = 7;
+int sudeste = 3;
+int sul = 5; 
+
+switch(regiao)
+{
+  case "norte":
+  Console.WriteLine($"A região é: {regiao}");
+  Console.WriteLine($"Prazo de entrega: {DateTime.Now.AddDays(norte).ToString("dd/MM/yyyy")} ");
+  break;
+
+  case "nordeste":
+  Console.WriteLine($"A região é: {regiao}");
+  Console.WriteLine($"Prazo de entrega: {DateTime.Now.AddDays(nordeste).ToString("dd/MM/yyyy")} ");
+  break;
+
+  case "centroOeste":
+  Console.WriteLine($"A região é: {regiao}");
+  Console.WriteLine($"Prazo de entrega: {DateTime.Now.AddDays(centroOeste).ToString("dd/MM/yyyy")} ");
+  break;
+
+  case "sudeste":
+  Console.WriteLine($"A região é: {regiao}");
+  Console.WriteLine($"Prazo de entrega: {DateTime.Now.AddDays(sudeste).ToString("dd/MM/yyyy")} ");
+  break;
+
+  case "sul":
+  Console.WriteLine($"A região é: {regiao}");
+  Console.WriteLine($"Prazo de entrega: {DateTime.Now.AddDays(sul).ToString("dd/MM/yyyy")} ");
+  break;
+
+  default:
+  Console.WriteLine("Região inválida");
+  break;
+}
+
+Console.WriteLine("===================");
+Console.WriteLine("  Sistema Combinado  ");
+Console.WriteLine("===================");
+
+string categoriaEletronicos = "Eletrônicos";
+Console.WriteLine("Categoria: " + categoriaEletronicos);
+
+string formaPagamento = "Credito";
+Console.WriteLine("Forma de Pagamento: " + formaPagamento);
+
+decimal valorProduto = 2000.00m; 
+Console.WriteLine("Valor do Produto: R$ " + valorProduto.ToString("C"));
+
+
+decimal taxaImposto = 0m;
+decimal taxaAjustePagamento = 0m;
+
+switch (categoriaEletronicos.ToLower())
+{
+ case "eletrônicos":
+ taxaImposto = 0.25m;
+ break;
+
+ case "alimentos":
+ taxaImposto = 0.05m;
+ break;
+ 
+ case "bebidas":
+ taxaImposto = 0.15m;
+ break;
+ default:
+ 
+ taxaImposto = 0.18m;
+ break;
+}
+
+switch (formaPagamento.ToLower())
+{
+  case "dinheiro":
+  taxaAjustePagamento = -0.10m;
+  break;
+  
+  case "pix":
+  taxaAjustePagamento = -0.05m;
+  break;
+  
+  case "credito":
+  taxaAjustePagamento = 0.05m;
+  break;
+  
+  case "debito":
+  taxaAjustePagamento = 0.00m;
+  break;
+}
+
+decimal valorComImposto = valorProduto + (valorProduto * taxaImposto);
+decimal valorFinal = valorComImposto + (valorComImposto * taxaAjustePagamento);
+
+Console.WriteLine($"Taxa de Imposto aplicada: {taxaImposto.ToString("P")}");
+Console.WriteLine($"Valor com Imposto: {valorComImposto.ToString("C")}");
+Console.WriteLine($"Ajuste de Pagamento: {taxaAjustePagamento.ToString("P")}");
+Console.WriteLine("===================");
+Console.WriteLine($"VALOR FINAL A PAGAR: {valorFinal.ToString("C")}");
+Console.WriteLine("===================");
+    
