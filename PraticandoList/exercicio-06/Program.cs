90ae73fd5df82ea2a1ad0bb37ba72fa58636935a
guilhermesaldanha02 <guilhermesaldanha007@gmail.com>
2026-01-21 21:48:48 -0300

    adicionado 2 exercicios na pasta Praticandolist

---
+++ PraticandoList/exercicio-06/Program.cs
@@ -0,0 +1,55 @@
+﻿List<string> tarefas = new List<string>();
+
+while (true)
+{
+  Console.WriteLine("\n--- MENU ---");
+  Console.WriteLine("1 - Adicionar");
+  Console.WriteLine("2 - Remover");
+  Console.WriteLine("3 - Listar");
+  Console.WriteLine("4 - Sair");
+
+  Console.WriteLine("Escolha uma opção:");
+  string opcao = Console.ReadLine();
+
+  switch (opcao)
+  {
+    case "1":
+      Console.WriteLine("Digite um nome:");
+      string nome = Console.ReadLine();
+      tarefas.Add(nome);
+      break;
+
+    case "2":
+      Console.WriteLine("Digite um nome para remover:");
+      string nomeRemover = Console.ReadLine();
+
+      bool conseguiuRemover = tarefas.Remove(nomeRemover);
+
+      if (conseguiuRemover)
+      {
+        Console.WriteLine("Item removido com sucesso!");
+      }
+      else
+      {
+        Console.WriteLine("Item não encontrado (verifique maiúsculas/minúsculas).");
+      }
+      break;
+
+    case "3":
+      Console.WriteLine("--- Lista de Tarefas ---");
+
+      for (int i = 0; i < tarefas.Count; i++)
+      {
+        Console.WriteLine($"{i} - {tarefas[i]}");
+      }
+      break;
+
+    case "4":
+      Console.WriteLine("Saindo...");
+      return;
+
+    default:
+      Console.WriteLine("Opção inválida!");
+      break;
+  }
+}
---
+++ PraticandoList/exercicio-07/Program.cs
@@ -0,0 +1,53 @@
+﻿List<string> playlist = new List<string>();
+
+while (true)
+{
+  Console.WriteLine("1-Adicionar");
+  Console.WriteLine("2-Remover");
+  Console.WriteLine("3-Ver Fila");
+  Console.WriteLine("4-Sair");
+
+  Console.WriteLine("Escolha um numero:");
+  string opcao = Console.ReadLine();
+
+  switch (opcao)
+  {
+    case "1":
+      Console.WriteLine("Digite o nome de uma musica:");
+      string Adicionar = Console.ReadLine();
+      playlist.Add(Adicionar);
+      break;
+
+    case "2":
+      Console.WriteLine("Digite o nome de uma musica:");
+      string nomeRemover = Console.ReadLine();
+
+      bool conseguiuRemover = playlist.Remove(nomeRemover);
+      if (conseguiuRemover)
+      {
+        Console.WriteLine("Musica removida com sucesso!");
+      }
+      else
+      {
+        Console.WriteLine("Item não encontrado (verifique maiúsculas/minúsculas).");
+      }
+      break;
+
+    case "3":
+      Console.WriteLine("--- Lista de Musicas ---");
+
+      for (int i = 0; i < playlist.Count; i++)
+      {
+        Console.WriteLine($"{i} - {playlist[i]}");
+      }
+      break;
+
+
+    case "4":
+      Console.WriteLine("Saindo...");
+      return;
+
+    default:
+      Console.WriteLine("Opção inválida!");
+  }
+}
