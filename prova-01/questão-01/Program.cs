da7aeca58e9d15b448099e12b87c1d6d2a969437
GuilhermeSaldanha02 <guilhermesaldanha007@gmail.com>
2026-01-29 12:49:36 -0300

    adicionando prova-01 arquivos questão 01 - 02

---
+++ prova-01/questão-01/Program.cs
@@ -0,0 +1,90 @@
+﻿List<string> produtos = [];
+List<decimal> precos = [];
+List<int> quantidades = [];
+
+decimal valorTotalEstoque = 0;
+decimal maiorPreco = 0;
+string nomeProdutoMaisCaro = "";
+int menorQuantidade = int.MaxValue;
+string nomeProdutoMenorEstoque = "";
+
+for (int i = 0; i < 5; i++)
+{
+
+    string nome = LerTexto("Digite o nome do produto:");
+    produtos.Add(nome);
+
+
+    decimal preco = LerDecimal("Digite o preço do produto:");
+    precos.Add(preco);
+
+    int quantidade = LerInt("Digite a quantidade em estoque:");
+    quantidades.Add(quantidade);
+}
+
+for (int i = 0; i < produtos.Count; i++)
+{
+    valorTotalEstoque += precos[i] * quantidades[i];
+
+
+    if (precos[i] > maiorPreco)
+    {
+        maiorPreco = precos[i];
+        nomeProdutoMaisCaro = produtos[i];
+    }
+
+
+    if (quantidades[i] < menorQuantidade)
+    {
+        menorQuantidade = quantidades[i];
+        nomeProdutoMenorEstoque = produtos[i];
+    }
+}
+Console.WriteLine("\n=== RELATÓRIO DE PRODUTOS ===");
+Console.WriteLine($"Valor Total em Estoque: R$ {valorTotalEstoque}");
+Console.WriteLine($"Produto Mais Caro: {nomeProdutoMaisCaro} (R$ {maiorPreco})");
+Console.WriteLine($"Produto com Menor Estoque: {nomeProdutoMenorEstoque} ({menorQuantidade} un)");
+
+
+string LerTexto(string mensagem)
+{
+    Console.WriteLine(mensagem);
+    string entrada = Console.ReadLine();
+
+    while (string.IsNullOrWhiteSpace(entrada))
+    {
+        Console.WriteLine("Erro! O campo não pode ser vazio. Tente de novo:");
+        entrada = Console.ReadLine();
+    }
+    return entrada;
+}
+
+decimal LerDecimal(string mensagem)
+{
+    Console.WriteLine(mensagem);
+    string entrada = Console.ReadLine();
+    decimal numero;
+
+
+    while (!decimal.TryParse(entrada, out numero) || numero <= 0)
+    {
+        Console.WriteLine("Erro! Digite um valor numérico maior que zero.");
+        entrada = Console.ReadLine();
+    }
+    return numero;
+}
+
+int LerInt(string mensagem)
+{
+    Console.WriteLine(mensagem);
+    string entrada = Console.ReadLine();
+    int numero;
+
+
+    while (!int.TryParse(entrada, out numero) || numero < 0)
+    {
+        Console.WriteLine("Erro! Digite um número inteiro válido (não negativo).");
+        entrada = Console.ReadLine();
+    }
+    return numero;
+}
---
+++ prova-01/questão-02/Program.cs
@@ -0,0 +1,84 @@
+﻿List<string> alunos = [];
+List<int> nota1 = [];
+List<int> nota2 = [];
+List<int> nota3 = [];
+
+
+int contatorAprovado = 0;
+int contadorRecuperacao = 0;
+int contadorReprovado = 0;
+decimal media;
+int totalNotas;
+
+for (int i = 1; i < 6; i++)
+{
+    string nome = LerTexto("Digite o nome do aluno:");
+    alunos.Add(nome);
+
+    int primeiraNota = LerInt($"Digite a nota 1:");
+    nota1.Add(primeiraNota);
+
+    int segundaNota = LerInt($"Digite a nota 2:");
+    nota2.Add(segundaNota);
+
+    int teceiraNota = LerInt($"Digite a nota 3:");
+    nota3.Add(teceiraNota);
+}
+
+Console.WriteLine("=== BOLETIM DA TURMA ===");
+for (int i = 0; i < alunos.Count; i++)
+{
+    totalNotas = nota1[i] + nota2[i] + nota3[i];
+    media = (decimal)totalNotas / 3;
+
+    string situacao = "";
+
+    if (media >= 7)
+    {
+        situacao = "Aprovado";
+        contatorAprovado++;
+    }
+    else if (media >= 5)
+    {
+        situacao = "Recuperação";
+        contadorRecuperacao++;
+    }
+    else
+    {
+        situacao = "Reprovado";
+        contadorReprovado++;
+
+    }
+    Console.WriteLine($"Aluno: {alunos[i]} | Média: {media:F1} | Status: {situacao}");
+}
+Console.WriteLine($"Total Aprovados: {contatorAprovado}");
+Console.WriteLine($"Total em recuperação: {contadorRecuperacao}");
+Console.WriteLine($"Total Reprovados: {contadorReprovado}");
+
+string LerTexto(string mensagem)
+{
+    Console.WriteLine(mensagem);
+    string entrada = Console.ReadLine();
+
+    while (string.IsNullOrWhiteSpace(entrada))
+    {
+        Console.WriteLine("Erro! O campo não pode ser vazio. Tente de novo:");
+        entrada = Console.ReadLine();
+    }
+    return entrada;
+}
+
+int LerInt(string mensagem)
+{
+    Console.WriteLine(mensagem);
+    string entrada = Console.ReadLine();
+    int numero;
+
+
+    while (!int.TryParse(entrada, out numero) || numero < 0 || numero > 10)
+    {
+        Console.WriteLine("Erro! Digite um número inteiro válido (não negativo).");
+        entrada = Console.ReadLine();
+    }
+    return numero;
+}
